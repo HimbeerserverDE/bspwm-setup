38edc5694baf3628778fbdb86f36645a7c4d37d5
HimbeerserverDE <himbeerserverde@gmail.com>
2022-04-23 22:57:39 +0200

    Add installer scripts

---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+function command_exists {
+	command -v $1 &> /dev/null
+}
+
+set -x
+
+if [ $UID -ne 0 ]; then
+	if command_exists sudo; then
+		SUDO="sudo"
+	elif command_exists doas; then
+		SUDO="doas"
+	fi
+fi
+
+if command_exists apt; then
+	wget -O- https://updates.signal.org/desktop/apt/keys.asc | $SUDO apt-key add -
+	echo "deb [arch=$(dpkg --print-architecture)] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+
+	$SUDO apt update
+	$SUDO apt install -y \
+		bspwm \
+		xorg \
+		polybar \
+		i3lock-fancy \
+		feh \
+		picom \
+		lua5.3 \
+		vlc \
+		firefox-esr \
+		thunderbird \
+		signal-desktop
+	$SUDO apt purge -y \
+		lemonbar # automatically installed but unwanted
+else
+	echo "Your distro is not supported."
+	exit 1
+fi
+
+mkdir -p ~/.config/bspwm
+ln -s ${PWD}/bspwmrc ~/.config/bspwm/bspwmrc
+
+mkdir -p ~/.config/sxhkd
+ln -s ${PWD}/sxhkdrc ~/.config/sxhkd/sxhkdrc
+
+ln -s ${PWD}/polybar ~/.config/polybar
+
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | bash
+
+mkdir -p ~/.config/kitty
+ln -s ${PWD}/kitty.conf ~/.config/kitty/kitty.conf
+
+mkdir -p ~/.config
+ln -s ${PWD}/picom.conf ~/.config/picom.conf
+
+./shell_only.sh
---
+++ shell_only.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function command_exists {
+	command -v $1 &> /dev/null
+}
+
+set -x
+
+if [ $UID -ne 0 ]; then
+	if command_exists sudo; then
+		SUDO="sudo"
+	elif command_exists doas; then
+		SUDO="doas"
+	fi
+fi
+
+if command_exists apt; then
+	$SUDO apt update
+	$SUDO apt install -y \
+		bat \
+		lolcat \
+		cowsay \
+		fortune \
+		zsh \
+		git \
+		figlet
+else
+	echo "Your distro is not supported."
+	exit 1
+fi
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+ln -s ${PWD}/bin ~/bin
+
+mkdir -p ~/.oh-my-zsh/themes
+ln -s ${PWD}/himbeer.zsh-theme ~/.oh-my-zsh/themes/himbeer.zsh-theme
+
+mkdir -p ~/
+ln -s ${PWD}/zshrc ~/.zshrc
+
+mkdir -p ~/
+ln -s ${PWD}/wallpaper.png ~/.wallpaper
