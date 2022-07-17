14673fcdc5f8e9bcdb9ce79eed1c7fa16159db88
HimbeerserverDE <himbeerserverde@gmail.com>
2022-07-17 09:55:00 +0200

    Deprecate  in favor of
    Closes #13

--- wait_for_monitor.sh
+++ wait_for_monitor.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 while [ true ]; do
-	if [ -n "`xrandr --listmonitors | grep 'DP-2'`" ]; then
+	if [ -n "$(xrandr --listmonitors | grep 'DP-2')" ]; then
 		bspc monitor 'DP-2' -n 2 -d 1 2 3 4 5 6 7 8 9
 		exit 0
 	fi
