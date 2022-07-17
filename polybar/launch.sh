248c7aea675692c342429616244efb58082de382
HimbeerserverDE <himbeerserverde@gmail.com>
2022-07-17 11:53:35 +0200

    Hide polybar below fullscreen windows even after in-place reload
    Once again something you might like @EliasFleckenstein03

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,3 +2,11 @@
 
 polybar --reload -q main -c "$HOME/.config/polybar/config.ini" &
 polybar --reload -q opt -c "$HOME/.config/polybar/config.ini" &
+
+while [ -e "$(pgrep -x polybar)" ]; do sleep 1; done
+sleep 2
+
+POLYBARS=$(pgrep -x polybar)
+for BAR in ${POLYBARS}; do
+	xdo below -t $(xdo id -n root) $(xdo id -p ${BAR})
+done
