5653269ca88c109d44ba611401de317de9e73c83
HimbeerserverDE <himbeerserverde@gmail.com>
2022-08-30 22:42:19 +0200

    Detect external monitor properly
    Fixes #26

--- init_monitor.sh
+++ init_monitor.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 while [ true ]; do
-	if [ -n "$(xrandr --listmonitors | grep 'DP-2')" ]; then
+	if [ "$(xrandr -q | grep 'DP-2' | awk '{print $2}')" == "connected" ]; then
+		xrandr --output 'DP-2' --right-of 'eDP-1'
 		bspc monitor 'DP-2' -n 2 -d 1 2 3 4 5 6 7 8 9
+		bspc wm -r
 		exit 0
 	fi
 
