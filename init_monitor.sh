67ddc5925edcc33b635f8cb0e19e82712036d3d4
HimbeerserverDE <himbeerserverde@gmail.com>
2022-08-31 11:03:24 +0200

    Don't reload the wm over and over again when an external monitor is connected

--- init_monitor.sh
+++ init_monitor.sh
@@ -4,7 +4,6 @@ while [ true ]; do
 	if [ "$(xrandr -q | grep 'DP-2' | awk '{print $2}')" == "connected" ]; then
 		xrandr --output 'DP-2' --right-of 'eDP-1'
 		bspc monitor 'DP-2' -n 2 -d 1 2 3 4 5 6 7 8 9
-		bspc wm -r
 		exit 0
 	fi
 
