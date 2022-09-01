4a65242f6b51cfe89c0d454752e08d66f7c7f199
HimbeerserverDE <himbeerserverde@gmail.com>
2022-09-01 12:03:05 +0200

    Fix wm-restack, dirty external script is no longer needed

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -17,11 +17,3 @@
 
 polybar --reload -q main -c "$HOME/.config/polybar/config.ini" &
 polybar --reload -q opt -c "$HOME/.config/polybar/config.ini" &
-
-while [ -z "$(pgrep -x polybar)" ]; do sleep 1; done
-sleep 2
-
-POLYBARS=$(pgrep -x polybar)
-for BAR in ${POLYBARS}; do
-	xdo below -t $(xdo id -n root) $(xdo id -p ${BAR})
-done
