1d4cb5e40977692a1bfc1b470125de8df9973b03
HimbeerserverDE <himbeerserverde@gmail.com>
2022-09-02 13:43:26 +0200

    set -e

--- install.sh
+++ install.sh
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+set -e
+
 echo -e "\e[1m\e[1;31mMAKE SURE YOU ARE IN THE DIRECTORY THIS SCRIPT IS LOCATED IN!"
 echo -e "\e[0m\e[1;31mIf you're sure you are in the correct dir, press Enter."
 read
