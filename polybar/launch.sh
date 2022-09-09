953f598c33ca74411616a44d4540a56f81d8dbdb
HimbeerserverDE <himbeerserverde@gmail.com>
2022-09-09 12:45:59 +0200

    $HOME -> ${HOME}

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-polybar --reload -q main -c "$HOME/.config/polybar/config.ini" &
-polybar --reload -q opt -c "$HOME/.config/polybar/config.ini" &
+polybar --reload -q main -c "${HOME}/.config/polybar/config.ini" &
+polybar --reload -q opt -c "${HOME}/.config/polybar/config.ini" &
 
-polybar --reload -q tray -c "$HOME/.config/polybar/config.ini" &
+polybar --reload -q tray -c "${HOME}/.config/polybar/config.ini" &
