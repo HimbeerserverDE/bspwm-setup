29a03fbf2c9b4bed78f4e91bda06cfc3d465a3f8
HimbeerserverDE <himbeerserverde@gmail.com>
2023-02-16 18:38:45 +0100

    x11 update: remove - from monitor names

--- init_monitor.sh
+++ init_monitor.sh
@@ -16,9 +16,9 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 while [ true ]; do
-	if [ "$(xrandr -q | grep 'DP-2' | awk '{print $2}')" == "connected" ]; then
-		xrandr --output 'DP-2' --right-of 'eDP-1'
-		bspc monitor 'DP-2' -n 2 -d 1 2 3 4 5 6 7 8 9
+	if [ "$(xrandr -q | grep 'DP2' | awk '{print $2}')" == "connected" ]; then
+		xrandr --output 'DP2' --right-of 'eDP1'
+		bspc monitor 'DP2' -n 2 -d 1 2 3 4 5 6 7 8 9
 		exit 0
 	fi
 
