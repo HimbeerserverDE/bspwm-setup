438fec6aa8c24adbb6eb9315de6c8a3e7c879ec3
HimbeerserverDE <himbeerserverde@gmail.com>
2023-02-19 17:12:17 +0100

    wait for pipewire to initialize before launching polybar

--- polybar/normal/launch.sh
+++ polybar/normal/launch.sh
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+sleep 1
+
 polybar --reload -q main -c "${HOME}/.config/polybar/config.ini" &
 polybar --reload -q opt -c "${HOME}/.config/polybar/config.ini" &
 
