464cfebc6f4bd53f2959b3fe9b6e991bb271b002
Himbeer <himbeer@disroot.org>
2024-10-19 15:18:19 +0200

    Switch to /tmp as the download directory
    Downloads always need to be moved anyway if they are important. Otherwise they
    can be deleted. Storing them in /tmp makes this more convenient since manual
    deletion is no longer required. Moving downloads to a more permanent location
    will not change. This is only safe on single-user systems due to the
    permissions on the /tmp directory.

--- install.sh
+++ install.sh
@@ -113,7 +113,7 @@ ${SUDO} usermod -aG seat ${USER}
 ${SUDO} usermod -aG video ${USER}
 
 xdg-user-dirs-update --set DESKTOP ~
-xdg-user-dirs-update --set DOWNLOAD ~/downloads
+xdg-user-dirs-update --set DOWNLOAD /tmp
 xdg-user-dirs-update --set TEMPLATES ~
 xdg-user-dirs-update --set PUBLICSHARE ~
 xdg-user-dirs-update --set DOCUMENTS ~/documents
